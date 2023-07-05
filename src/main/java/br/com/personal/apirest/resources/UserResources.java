81cb65d5801c1677115180e245e886e34bd52e38
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-05 15:52:07 -0300

    user resource findById

--- src/main/java/br/com/personal/apirest/resources/UserResources.java
+++ src/main/java/br/com/personal/apirest/resources/UserResources.java
@@ -1,7 +1,9 @@
 package br.com.personal.apirest.resources;
 
+import br.com.personal.apirest.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResources {
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(new User(1, "Valdir", "val@email.com", "123"));
+    }
 }
+
