78f0fa69f55fb08f3f6aa43c9c71dbdfed119624
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-05 15:39:56 -0300

    branch dev

---
+++ src/main/java/br/com/personal/apirest/resources/UserResources.java
@@ -0,0 +1,13 @@
+package br.com.personal.apirest.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResources {
+
+
+}
