140f676f86f64bd54338e60314d8fdbbd1b12e06
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 11:06:17 -0300

    endpoint findall user

--- src/main/java/br/com/personal/apirest/services/UserService.java
+++ src/main/java/br/com/personal/apirest/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.personal.apirest.services;
 
 import br.com.personal.apirest.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
