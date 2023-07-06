2e48fa1568fdd9f915d14f18b71bb8ac522031fe
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 13:49:56 -0300

    teste userservice create com excecao

--- src/test/java/br/com/personal/apirest/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/personal/apirest/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.personal.apirest.services.impl;
 import br.com.personal.apirest.domain.User;
 import br.com.personal.apirest.domain.dto.UserDTO;
 import br.com.personal.apirest.repositories.UserRepository;
+import br.com.personal.apirest.services.exceptions.DataIntegratyViolationException;
 import br.com.personal.apirest.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,8 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 class UserServiceImplTest {
@@ -97,6 +97,20 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
         assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenCreateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try {
+            optionalUser.get().setId(2);
+            service.create(userDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("Email já cadastrado no sistema", ex.getMessage());
+        }
+
     }
 
     @Test
