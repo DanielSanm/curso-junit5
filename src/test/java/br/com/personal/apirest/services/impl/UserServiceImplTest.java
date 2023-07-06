2f27ea65880a7d0682166a6ac188b8e0db8a8396
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 13:59:40 -0300

    teste userservice update com excecao

--- src/test/java/br/com/personal/apirest/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/personal/apirest/services/impl/UserServiceImplTest.java
@@ -29,6 +29,7 @@ class UserServiceImplTest {
     public static final String PASSWORD = "123";
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
     public static final int INDEX = 0;
+    public static final String EMAIL_JA_CADASTRADO = "Email já cadastrado no sistema";
     @InjectMocks
     private UserServiceImpl service;
     @Mock
@@ -108,7 +109,7 @@ class UserServiceImplTest {
             service.create(userDTO);
         } catch (Exception ex) {
             assertEquals(DataIntegratyViolationException.class, ex.getClass());
-            assertEquals("Email já cadastrado no sistema", ex.getMessage());
+            assertEquals(EMAIL_JA_CADASTRADO, ex.getMessage());
         }
 
     }
@@ -125,6 +126,20 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
         assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenUpdateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try {
+            optionalUser.get().setId(2);
+            service.update(userDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals(EMAIL_JA_CADASTRADO, ex.getMessage());
+        }
+
     }
 
     @Test
