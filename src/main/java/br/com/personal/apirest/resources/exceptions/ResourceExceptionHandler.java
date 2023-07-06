156dfd3c1f96cec4d7baed79fa18f25d2963576f
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 16:15:27 -0300

    test handler excecao objectnotfound

--- src/main/java/br/com/personal/apirest/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/personal/apirest/resources/exceptions/ResourceExceptionHandler.java
@@ -3,8 +3,6 @@ package br.com.personal.apirest.resources.exceptions;
 import br.com.personal.apirest.services.exceptions.DataIntegratyViolationException;
 import br.com.personal.apirest.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.coyote.Response;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ResourceExceptionsHandler {
+public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
