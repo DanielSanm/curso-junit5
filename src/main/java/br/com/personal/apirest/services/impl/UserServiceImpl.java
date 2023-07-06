4d5517d2d81c1fb7f0a051cfc50c22d4ece04ee9
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 10:43:58 -0300

    excecoes para objeto nao encotrado

--- src/main/java/br/com/personal/apirest/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/personal/apirest/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.personal.apirest.services.impl;
 import br.com.personal.apirest.domain.User;
 import br.com.personal.apirest.repositories.UserRepository;
 import br.com.personal.apirest.services.UserService;
+import br.com.personal.apirest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
