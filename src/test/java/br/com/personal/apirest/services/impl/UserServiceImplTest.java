14c689dc5a864505a260c385010004d4b815e586
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 14:06:04 -0300

    teste userservice delete

--- src/test/java/br/com/personal/apirest/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/personal/apirest/services/impl/UserServiceImplTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
 
@@ -143,7 +143,13 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
+
     }
 
     private void startUser() {
