c754d1cf8102c4f6f2163ef2019f3331d80c1bd9
Daniel Elias <69224429+DanielSanm@users.noreply.github.com>
2023-07-06 10:29:38 -0300

    criando banco h2

---
+++ src/main/java/br/com/personal/apirest/config/LocalConfig.java
@@ -0,0 +1,28 @@
+package br.com.personal.apirest.config;
+
+
+import br.com.personal.apirest.domain.User;
+import br.com.personal.apirest.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB() {
+        User u1 = new User(null, "Valdir", "valdir@email.com", "123");
+        User u2 = new User(null, "Luiz", "luix@email.com", "123");
+
+        repository.saveAll(List.of(u1, u2));
+
+    }
+}
